59e9dc697828ad95b09bd4ba6d926239c1f0d7c0
ronenniv <ronen.niv@gmail.com>
2021-01-08 00:08:19 -0500

    add option to run with files from Downloads directory

--- stocks_orders.sh
+++ stocks_orders.sh
@@ -3,7 +3,16 @@ clear
 
 echo "Running main.py"
 
-python3 main.py $@
+if [ $# -eq 0 ]
+then
+  cost_file=`ls -t ~/Downloads/ExportData*.csv | tail -1`
+  order_file=`ls -t ~/Downloads/ExportData*.csv | head -1`
+  echo "$cost_file"
+  python3 main.py -c $cost_file -o $order_file -csv yes
+else
+  python3 main.py $@
+fi
+
 result=$?
 
 if [ $result -eq 0 ]
