31094787b87f3c09ede858d0d78e762cbeceb964
ronenniv <ronen.niv@gmail.com>
2021-01-09 17:54:03 -0500

    improved error checking when no params

--- stocks_orders.sh
+++ stocks_orders.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
-clear
-
-echo "Running main.py"
 
 if [ $# -eq 0 ]
 then
+  echo "No arguments. Searching for existing csv files in ~/Downloads."
   cost_file=`ls -t ~/Downloads/ExportData*.csv | tail -1`
+  if [ "$cost_file" = "" ]
+  then
+    echo "Error: No file found with pattern ExportData*.csv."
+    exit 1
+  fi
   order_file=`ls -t ~/Downloads/ExportData*.csv | head -1`
-  echo "$cost_file"
+  if [ "$cost_file" = "$order_file" ]
+  then
+    echo "Error: only one csv file found. Missing files."
+    exit 1
+  fi
+  echo "execute -c $cost_file -o $order_file"
   python3 main.py -c $cost_file -o $order_file -csv yes
 else
   python3 main.py $@
@@ -17,7 +25,7 @@ result=$?
 
 if [ $result -eq 0 ]
 then
-  echo "Completed successfully"
+  echo "Completed successfully!!!"
 else
-  echo "Completed with errors"
+  echo "Completed with errors!!!"
 fi
