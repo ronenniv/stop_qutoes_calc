a48911bf3c1b572d0938e77411f608ab401d6f3a
ronenniv <ronen.niv@gmail.com>
2021-01-14 20:36:02 -0500

    update formatting

--- stocks_orders.sh
+++ stocks_orders.sh
@@ -3,22 +3,22 @@
 if [ $# -eq 0 ]
 then
   echo "No arguments. Searching for existing csv files in ~/Downloads."
-  cost_file=`ls -t ~/Downloads/ExportData*.csv | tail -1`
+  cost_file=$(ls -t ~/Downloads/ExportData*.csv | tail -1)
   if [ "$cost_file" = "" ]
   then
     echo "Error: No file found with pattern ExportData*.csv."
     exit 1
   fi
-  order_file=`ls -t ~/Downloads/ExportData*.csv | head -1`
+  order_file=$(ls -t ~/Downloads/ExportData*.csv | head -1)
   if [ "$cost_file" = "$order_file" ]
   then
     echo "Error: only one csv file found. Missing files."
     exit 1
   fi
   echo "execute -c $cost_file -o $order_file"
-  python3 main.py -c $cost_file -o $order_file -csv yes
+  python3 main.py -c "$cost_file" -o "$order_file" -csv yes
 else
-  python3 main.py $@
+  python3 main.py "$@"
 fi
 
 result=$?
@@ -26,8 +26,7 @@ result=$?
 if [ $result -eq 0 ]
 then
   echo "Completed successfully!!!"
-  exit 0
 else
   echo "Completed with errors!!!"
-  exit 1
 fi
+exit $result
